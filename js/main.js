d95ef896073ffb936ff6599861f287110ab0c60e
clydeli <clyde1008li@gmail.com>
2013-05-25 10:54:44 -0700

    try fix tourlist and tweak checkin

--- js/main.js
+++ js/main.js
@@ -8,11 +8,6 @@ $(document).on("pageinit", "#demo-page", function() {
 	$("#tourList").show();
 	
 	$(document).on("swiperight", "#demo-page", function(e) {
-		// We check if there is no open panel on the page because otherwise
-		// a swipe to close the left panel would also open the right panel (and
-		// v.v.).
-		// We do this by checking the data that the framework stores on the page
-		// element (panel: open).
 		if ($.mobile.activePage.jqmData("panel") !== "open") {
 			if (e.type === "swiperight") {
 				$("#left-panel").panel("open");
@@ -27,10 +22,10 @@ $(document).on("pageinit", "#demo-page", function() {
 		updateTitle($(this).data('page'));
 		switch($(this).data('page')){
 			case "nfcCheckIn":
-				tourout.nfc.init();
+				tourout.checkin.init();
 				break;
 			case "tourList":
-				tourout.tourist.init();
+				tourout.tourlist.init();
 				break;
 		}
 	});
