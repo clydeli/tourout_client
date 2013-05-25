53e71ece9bd6c8323413b59b9a1b95b131654bcb
seanxiaoxiao <seanxiaoxiao@hotmail.com>
2013-05-25 07:49:20 -0700

    Frameworks

---
+++ js/main.js
@@ -0,0 +1,38 @@
+function updateTitle(title) {
+	$("#head-title").text(title);
+}
+
+$(document).on("pageinit", "#demo-page", function() {
+
+	$(".innerPages").hide();
+	$("#tourList").show();
+	
+	$(document).on("swiperight", "#demo-page", function(e) {
+		// We check if there is no open panel on the page because otherwise
+		// a swipe to close the left panel would also open the right panel (and
+		// v.v.).
+		// We do this by checking the data that the framework stores on the page
+		// element (panel: open).
+		if ($.mobile.activePage.jqmData("panel") !== "open") {
+			if (e.type === "swiperight") {
+				$("#left-panel").panel("open");
+			}
+		}
+	});
+
+	$(document).on("click", "#left-panel li a", function(e) {
+		$(".innerPages").hide();
+		$("#" + $(this).data('page')).show();
+		$("#left-panel").panel("close");
+		updateTitle($(this).data('page'));
+	});
+	
+	function bindTourDetail() {
+		$("#tourList a").on("click", function() {
+			$("#right-panel").panel("open");
+		});
+	}
+	
+	bindTourDetail();
+});
+
