becea76024855056006be41afd0ed3772e653097
clydeli <clyde1008li@gmail.com>
2013-05-25 11:46:47 -0700

    minor progress on checkin

--- js/checkin.js
+++ js/checkin.js
@@ -10,12 +10,14 @@ tourout.checkin = (function() {
 		
 		sendVisitorInfo = function(){
 			tourout.nfc.sendTextNDEF(
-				[JSON.strigify(tourInfo)],
+				[JSON.stringify(tourInfo)],
 				{
 					success : function(){ console.log("good sent"); },
 					error : function(){ console.log("bad sent"); }
 				}
 			);
+			var popup = '<div data-role="popup" id="popup-received" data-theme="none" data-overlay-theme="a" data-corners="false" data-tolerance="15"> Checking... </div>';
+			$.mobile.activePage.append( popup ).trigger( "pagecreate" );
 		},
 	
 		checkInTour = function(){
@@ -28,12 +30,25 @@ tourout.checkin = (function() {
 					// do something
 				}
 			});
+		},
+		
+		nfcCallbacks = {
+			onattach : function(){ 
+				console.log("attached");
+			},
+			onreceive : function(message){ 
+				console.log("received");
+				console.log(message.records[0].text);
+				var msg = JSON.parse(message.records[0].text);
+				
+			}	
 		};
 		
 	
 	return {
 		init : function(){
-			tourout.nfc.init();
+			tourout.nfc.init(nfcCallbacks);
+			sendVisitorInfo();
 		},
 		sendVisitorInfo : sendVisitorInfo
 	};
