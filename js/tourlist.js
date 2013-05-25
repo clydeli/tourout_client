48fce17c90b01073a28ff531fc959e223dbb6730
lakeskysea <lakeskysea@gmail.com>
2013-05-25 11:47:52 -0700

    tour list with server data

--- js/tourlist.js
+++ js/tourlist.js
@@ -3,14 +3,20 @@ var tourout = tourout || {};
 tourout.tourlist = (function() {
 	var
 		init = function(){
-			console.log("here!");
 			$.ajax({
 				url: "http://cmu-tourout.appspot.com/getTours",
 				type: 'GET',
 				dataType: 'json',
 				success: function (jsondata) {
-					console.log(jsondata);
-					// do something
+					var tourList = [];
+					 
+					  //$.each(jsondata, function(key, val) {
+					  for(var i=0; i<jsondata.length; ++i){					
+							tourList.push('<li><a href="#" data-tour-id="' + jsondata[i].key.id + '">' + jsondata[i].propertyMap.companyName + '</a></li>');
+					  }
+					  
+					  $('#tourListUl').html(tourList);
+					  $('#tourListUl').listview('refresh');
 				}
 			});
 		};
