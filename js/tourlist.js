e55717ce2492bb1100a536dafb4bcc014e66844b
lakeskysea <lakeskysea@gmail.com>
2013-05-25 14:23:24 -0700

    show my list

--- js/tourlist.js
+++ js/tourlist.js
@@ -20,15 +20,36 @@ tourout.tourlist = (function() {
 	var
 		init = function(){
 			$.ajax({
-				url: "http://cmu-tourout.appspot.com/getTours",
+				url: "http://cmu-tourout.appspot.com/myTours?visitor=Sean",
 				type: 'GET',
 				dataType: 'json',
 				success: function (jsondata) {
-					var tourList = [];
+					var finishedTours = [];
+					var pendingTours = []; 
 					 
-					  //$.each(jsondata, function(key, val) {
-					  for(var i=0; i<jsondata.length; ++i){					
-							tourList.push('<li><a href="#" data-tour-id="' + jsondata[i].key.id + '">' + jsondata[i].propertyMap.companyName + '</a></li>');
+					  for(var i=0; i<jsondata.length; ++i){
+						  if(jsondata[i].propertyMap.isFinished) {
+							  finishedTours.push('<li><a href="#" data-tour-id="' + jsondata[i].key.id + '">' + jsondata[i].propertyMap.companyName + '</a></li>');
+						  } else {
+							  pendingTours.push('<li><a href="#" data-tour-id="' + jsondata[i].key.id + '">' + jsondata[i].propertyMap.companyName + '</a></li>');
+						  }
+					  }
+					  
+					  var tourList = [];
+					  if(pendingTours.length > 0) {
+						  tourList.push('<li data-role="list-divider" >Pending Tours</li>');
+					  }
+					  
+					  for(var i=0; i<pendingTours.length; i++) {
+						  tourList.push(pendingTours[i]);
+					  }
+					  
+					  if(finishedTours.length > 0) {
+						  tourList.push('<li data-role="list-divider" >Finished Tours</li>');
+					  }
+					  
+					  for(var i=0; i<finishedTours.length; i++) {
+						  tourList.push(finishedTours[i]);
 					  }
 					  
 					  $('#tourListUl').html(tourList);
